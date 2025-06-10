444b3d982e66bb2fd55f6a65c07a5b6f9c611bbd
eldeebcode <eldeebcode@gmail.com>
2025-06-11 02:40:11 +0300

    File Edux Academy

---
+++ js/main.js
@@ -0,0 +1,12 @@
+    VANTA.NET({
+      el: "#background",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x30111,
+      backgroundColor: 0xf2f2f2
+    })
+
